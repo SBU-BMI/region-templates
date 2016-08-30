ec300f227ec16ec4f0ddfff9ed0ac6e31c2ac93a
Willian Junior <guns945@gmail.com>
2016-08-29 22:32:31 -0300

    Task code generator implemented. Still need to do Stage generation.

--- runtime/regiontemplates/examples/PipelineRTFS-NS-Diff-FGO/gen/gen.cpp
+++ runtime/regiontemplates/examples/PipelineRTFS-NS-Diff-FGO/gen/gen.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 string generate_header(Json::Value data);
 string generate_source(Json::Value data);
+string generate_tasks(Json::Value data, string &exec_task);
 string getTypeCast(string type);
 string getArgumentTypeCast(string type);
 void replace_multiple_string(string& str, string to_find, string to_replace);
@@ -149,12 +150,8 @@ string generate_source(Json::Value data) {
 	string commonArgsLoop = 		// $PCB_ARGS$
 		"\tint set_cout = 0;\n\tfor(int i=0; i<this->getArgumentsSize(); i++){\n";
 	string pcb_task_params;			// $PCB_TASK_PARAMS$
-	string task_params;				// $TASK_PARAMS$
 	string task_args;				// $TASK_ARGS$
-	string dr_delete;				// $INPUT_DR_DELETE$
-	string input_mat_dr;			// $INPUT_MAT_DR$
-	string output_mat_dr;			// $OUTPUT_MAT_DR$
-	string output_return;			// $OUTPUT_DR_RETURN$
+	string exec_task;
 
 	// generate DataRegion strings
 	for (int i=0; i<data["args"].size(); i++) {
@@ -186,12 +183,6 @@ string generate_source(Json::Value data) {
 					"std::to_string(" + dr_name + "_arg->getId()), 0, " + dr_name + 
 					"_arg->getId()));\n";
 
-				// generate DR cleaning for the destructor - $INPUT_DR_DELETE$
-				dr_delete += "\tif(" + dr_name + "_temp != NULL) delete " + dr_name + "_temp;\n";
-
-				// generate cv::Mat variables to hold input DR - $INPUT_MAT_DR$
-				input_mat_dr += "\tcv::Mat " + dr_name + " = this->" + 
-					dr_name + "_temp->getData();";
 			} 
 			// generate output string
 			else if (data["args"][i]["io"].asString().compare("output") == 0) {
@@ -219,12 +210,6 @@ string generate_source(Json::Value data) {
 					dr_name + "_arg->getId());\n\t\t\tinputRt->insertDataRegion(" + 
 					dr_name + ");\n";
 
-				// generate cv::Mat variables to hold input DR - $OUTPUT_MAT_DR$
-				output_mat_dr += "\tcv::Mat " + dr_name + ";";
-
-				// generate mat assignment to output DR string - $OUTPUT_DR_RETURN$
-				output_return += "\tthis->" + dr_name + "_temp->setData(" + dr_name + ");";
-
 			} else {
 				cout << "Malformed descriptor." << endl;
 				exit(-1);
@@ -232,9 +217,6 @@ string generate_source(Json::Value data) {
 
 			// generate DR args $PCB_TASK_PARAMS$
 			pcb_task_params += dr_name + ", ";
-
-			// generate DR args $TASK_PARAMS$
-			task_params += "DenseDataRegion2D* " + dr_name + "_temp, ";
 
 			// generate DR args $TASK_ARGS$
 			task_args += "\tthis->" + dr_name + "_temp = " + dr_name + "_temp;\n";
@@ -258,12 +240,6 @@ string generate_source(Json::Value data) {
 
 			// generate common args $PCB_TASK_PARAMS$
 			pcb_task_params += arg_name + ", ";
-
-			// generate common args $TASK_PARAMS$
-			task_params += getTypeCast(arg_type) + " " + arg_name + ", ";
-
-			// generate DR args $TASK_ARGS$
-			task_args += "\tthis->" + arg_name + " = " + arg_name + ";\n";
 		}
 	}
 
@@ -276,11 +252,8 @@ string generate_source(Json::Value data) {
 	// remove final comma from pcb_task_params
 	pcb_task_params.erase(pcb_task_params.length()-2, 2);
 
-	// remove final comma from task_params
-	task_params.erase(task_params.length()-2, 2);
-
-	// generate the command invocation string
-	string cmd = data["call"].asString();
+	// generate the tasks
+	string tasks = generate_tasks(data, exec_task);
 
 	/********************************************************/
 	/***************** Generate File String *****************/
@@ -295,8 +268,6 @@ string generate_source(Json::Value data) {
 	string source;
 	while(getline(&line, &length, source_template) != -1)
 		source += string(line);
-
-	size_t pos;
 
 
 	// $NAME$
@@ -326,28 +297,247 @@ string generate_source(Json::Value data) {
 	// $PCB_TASK_PARAMS$
 	replace_multiple_string(source, "$PCB_TASK_PARAMS$", pcb_task_params);
 
-	// $TASK_PARAMS$
-	replace_multiple_string(source, "$TASK_PARAMS$", task_params);
+	// $EXEC_TASK$
+	replace_multiple_string(source, "$EXEC_TASK$", exec_task);
 
-	// $TASK_ARGS$
-	replace_multiple_string(source, "$TASK_ARGS$", task_args);
-
-	// $INPUT_DR_DELETE$
-	replace_multiple_string(source, "$INPUT_DR_DELETE$", dr_delete);
-
-	// $INPUT_MAT_DR$
-	replace_multiple_string(source, "$INPUT_MAT_DR$", input_mat_dr);
-
-	// $OUTPUT_MAT_DR$
-	replace_multiple_string(source, "$OUTPUT_MAT_DR$", output_mat_dr);
-
-	// $CMD$
-	replace_multiple_string(source, "$CMD$", cmd);
-
-	// $OUTPUT_DR_RETURN$
-	replace_multiple_string(source, "$OUTPUT_DR_RETURN$", output_return);
+	// $TASKS$
+	replace_multiple_string(source, "$TASKS$", tasks);
 
 	return source;
+}
+
+string generate_tasks(Json::Value data, string &exec_task) {
+
+	string final_source;
+
+	// go through all tasks
+	for (int i=0; i<data["tasks"].size(); i++) {
+		string name = data["name"].asString() + to_string(i);
+		string type_check = "";
+		string task_args;
+		string input_dr_delete;
+		string input_mat_dr;
+		string output_mat_dr;
+		string cmd = data["tasks"][i]["call"].asString() + "(";
+		string call_args;
+		string output_dr_return;
+		string update_mat_dr;
+		string update_ints_args;
+		string resolve_deps;
+		string reusable_cond;
+		string task_size;
+		string task_serialize;
+		string task_deserialize;
+		string task_print;
+
+		if (i != data["tasks"].size()-1) {
+			type_check = "\tif (typeid(t) != typeid(Task" + data["name"].asString() + to_string(i+1) + 
+				")) {\n\t\tstd::cout << \"[Task" + data["name"].asString() + to_string(i+1) + 
+				"] \" << __FILE__ << \":\" << __LINE__ <<\" incompatible tasks.\" << std::endl;\n\t}\n";
+		}
+
+		// go through all args
+		for (int j=0; j<data["tasks"][i]["args"].size(); j++) {
+			call_args += data["tasks"][i]["args"][j]["name"].asString() + ", ";
+
+			if (data["tasks"][i]["args"][j]["type"].asString().compare("dr") == 0) {
+				task_args += "\t\tif (a->getName().compare(\"" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "\") == 0) {\n\t\t\tArgumentRT* " + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_arg;\n\t\t\t" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_arg = (ArgumentRT*)a;\n\t\t\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp = new DenseDataRegion2D();\n\t\t\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->setName(" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_arg->getName());\n\t\t\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->setId(std::to_string(" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_arg->getId()));\n\t\t\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->setVersion(" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_arg->getId());\n\t\t\tset_cout++;\n\t\t}\n\n";
+
+				reusable_cond += "\t\tthis->" + data["tasks"][i]["args"][j]["name"].asString() + "_temp->getName() == t->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->getName() &&\n\t\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->getId() == t->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->getId() &&\n\t\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->getVersion() == t->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->getVersion() &&\n";
+
+				task_size += "\t\tsizeof(int) + " + data["tasks"][i]["args"][j]["name"].asString() + 
+					"_temp->getName().length()*sizeof(char) + sizeof(int) +\n";
+
+				task_serialize += "\t// copy " + data["tasks"][i]["args"][j]["name"].asString() + " id\n\tint " + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_id = stoi(" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->getId());\n\tmemcpy(buff+serialized_bytes, &" + 
+					data["tasks"][i]["args"][j]["name"].asString() + 
+					"_id, sizeof(int));\n\tserialized_bytes+=sizeof(int);\n\n\t// copy " + 
+					data["tasks"][i]["args"][j]["name"].asString() + " name size\n\tint " + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name_size = " + 
+					data["tasks"][i]["args"][j]["name"].asString() + 
+					"_temp->getName().length();\n\tmemcpy(buff+serialized_bytes, &" + 
+					data["tasks"][i]["args"][j]["name"].asString() + 
+					"_name_size, sizeof(int));\n\tserialized_bytes+=sizeof(int);\n\n\t// copy " + 
+					data["tasks"][i]["args"][j]["name"].asString() + " name\n\tmemcpy(buff+serialized_bytes, " + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->getName().c_str(), " + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name_size*sizeof(char));\n\tserialized_bytes+=" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name_size*sizeof(char);\n\n";
+
+				task_deserialize += "\t// create the " + data["tasks"][i]["args"][j]["name"].asString() + "\n\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp = new DenseDataRegion2D();\n\n\t// extract " + 
+					data["tasks"][i]["args"][j]["name"].asString() + " id\n\tint " + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_id = ((int*)(buff+deserialized_bytes))[0];\n\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->setId(to_string(" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_id));\n\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->setVersion(" + 
+					data["tasks"][i]["args"][j]["name"].asString() + 
+					"_id);\n\tdeserialized_bytes += sizeof(int);\n\n\t// extract " + 
+					data["tasks"][i]["args"][j]["name"].asString() + " name size\n\tint " + 
+					data["tasks"][i]["args"][j]["name"].asString() + 
+					"_name_size = ((int*)(buff+deserialized_bytes))[0];" + 
+					"\n\tdeserialized_bytes += sizeof(int);\n\n\t// copy " + 
+					data["tasks"][i]["args"][j]["name"].asString() + " name\n\tchar " + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name[" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name_size+1];\n\t" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name[" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name_size] = \'\\0\';\n\tmemcpy(" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name, buff+deserialized_bytes, sizeof(char)*" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name_size);\n\tdeserialized_bytes += sizeof(char)*" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name_size;\n\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_temp->setName(" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "_name);\n\n";
+
+				if (data["tasks"][i]["args"][j]["io"].asString().compare("input") == 0) {
+					input_dr_delete += "\tif(" + data["tasks"][i]["args"][j]["name"].asString() + "_temp != NULL) delete " + 
+						data["tasks"][i]["args"][j]["name"].asString() + "_temp;\n";
+
+					input_mat_dr += "\tcv::Mat " + data["tasks"][i]["args"][j]["name"].asString() + ";\n";
+
+					update_mat_dr += "\t" + data["tasks"][i]["args"][j]["name"].asString() + 
+						"_temp = dynamic_cast<DenseDataRegion2D*>(rt->getDataRegion(this->" + 
+						data["tasks"][i]["args"][j]["name"].asString() + "_temp->getName(),\n\t\tthis->" + 
+						data["tasks"][i]["args"][j]["name"].asString() + "_temp->getId(), 0, stoi(this->" + 
+						data["tasks"][i]["args"][j]["name"].asString() + "_temp->getId())));\n";
+				} else {
+					output_mat_dr += "\tcv::Mat " + data["tasks"][i]["args"][j]["name"].asString() + " = this->" + 
+						data["tasks"][i]["args"][j]["name"].asString() + "_temp->getData();\n";
+
+					output_dr_return += "\t" + data["tasks"][i]["args"][j]["name"].asString() + 
+						" = new cv::Mat(" + data["tasks"][i]["args"][j]["name"].asString() + "_temp);\n";
+
+					update_mat_dr += "rt->insertDataRegion(this->" + 
+						data["tasks"][i]["args"][j]["name"].asString() + "_temp);\n";
+				}
+			} else {
+				task_args += "\t\tif (a->getName().compare(\"" + 
+					data["tasks"][i]["args"][j]["name"].asString() + "\") == 0) {\n\t\t\tthis->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + " = (" + 
+					getTypeCast(data["tasks"][i]["args"][j]["type"].asString()) + ")(" + 
+					getArgumentTypeCast(data["tasks"][i]["args"][j]["type"].asString()) + 
+					"a)->getArgValue();\n\t\t\tset_cout++;\n\t\t}\n\n";
+
+				reusable_cond += "\t\tthis->" + data["tasks"][i]["args"][j]["name"].asString() + " == t->" + 
+					data["tasks"][i]["args"][j]["name"].asString() + " &&\n";
+
+				task_size += "\t\tsizeof(" + getTypeCast(data["tasks"][i]["args"][j]["type"].asString()) + 
+					") +\n";
+
+				task_serialize += "\t// copy field " + data["tasks"][i]["args"][j]["name"].asString() + 
+					"\n\tmemcpy(buff+serialized_bytes, &" + 
+					data["tasks"][i]["args"][j]["name"].asString() + ", sizeof(" + 
+					getTypeCast(data["tasks"][i]["args"][j]["type"].asString()) + "));\n\tserialized_bytes+=sizeof(" + 
+					getTypeCast(data["tasks"][i]["args"][j]["type"].asString()) + ");\n\n";
+
+				task_deserialize += "\t// extract field " + data["tasks"][i]["args"][j]["name"].asString() + 
+					"\n\tthis->" + data["tasks"][i]["args"][j]["name"].asString() + " = ((" + 
+					getTypeCast(data["tasks"][i]["args"][j]["type"].asString()) + 
+					"*)(buff+deserialized_bytes))[0];\n\tdeserialized_bytes += sizeof(" + 
+					getTypeCast(data["tasks"][i]["args"][j]["type"].asString()) + ");\n\n";
+
+				task_print += "\tcout << \"\t\t\tminSizePl: \" << " + 
+					data["tasks"][i]["args"][j]["name"].asString() + " << endl;\n";
+			}
+		}
+
+		// go through all interstage args
+		for (int j=0; j<data["tasks"][i]["interstage_args"].size(); j++) {
+			call_args += data["tasks"][i]["interstage_args"][j]["name"].asString() + ", ";
+
+			if (data["tasks"][i]["interstage_args"][j]["io"].asString().compare("input") == 0) {
+				update_ints_args += "\tthis->" + data["tasks"][i]["interstage_args"][j]["name"].asString() + 
+					" = ((Task$NAME$*)t)->" + data["tasks"][i]["interstage_args"][j]["name"].asString() + ";\n";
+
+				resolve_deps += "\tthis->" + data["tasks"][i]["interstage_args"][j]["name"].asString() + 
+					" = &((Task$NAME$*)t)->" + data["tasks"][i]["interstage_args"][j]["name"].asString() + ";\n";
+			}
+		}
+
+		// remove cmd last comma and add a parentesis
+		call_args.erase(call_args.length()-2, 2);
+		cmd += call_args;
+		cmd += ");";
+
+		// open source_template file
+		char* line;
+		size_t length=0;
+		FILE* source_template = fopen("source_task_template", "r");
+
+		// concat all source_template lines
+		string source;
+		while(getline(&line, &length, source_template) != -1)
+			source += string(line);
+
+		// $TASK_ARGS$
+		replace_multiple_string(source, "$TASK_ARGS$", task_args);
+
+		// $INPUT_DR_DELETE$
+		replace_multiple_string(source, "$INPUT_DR_DELETE$", input_dr_delete);
+
+		// $INPUT_MAT_DR$
+		replace_multiple_string(source, "$INPUT_MAT_DR$", input_mat_dr);
+
+		// $OUTPUT_MAT_DR$
+		replace_multiple_string(source, "$OUTPUT_MAT_DR$", output_mat_dr);
+
+		// $CMD$
+		replace_multiple_string(source, "$CMD$", cmd);
+
+		// $OUTPUT_DR_RETURN$
+		replace_multiple_string(source, "$OUTPUT_DR_RETURN$", output_dr_return);
+
+		// $UPDATE_MAT_DR$
+		replace_multiple_string(source, "$UPDATE_MAT_DR$", update_mat_dr);
+
+		// $UPDATE_INTS_ARGS$
+		replace_multiple_string(source, "$UPDATE_INTS_ARGS$", update_ints_args);
+
+		// $RESOLVE_DEPS$
+		replace_multiple_string(source, "$RESOLVE_DEPS$", resolve_deps);
+
+		// $REUSABLE_COND$
+		replace_multiple_string(source, "$REUSABLE_COND$", reusable_cond);
+
+		// $TASK_SIZE$
+		replace_multiple_string(source, "$TASK_SIZE$", task_size);
+
+		// $TASK_SERIALIZE$
+		replace_multiple_string(source, "$TASK_SERIALIZE$", task_serialize);
+
+		// $TASK_DESERIALIZE$
+		replace_multiple_string(source, "$TASK_DESERIALIZE$", task_deserialize);
+
+		// $TASK_PRINT$
+		replace_multiple_string(source, "$TASK_PRINT$", task_print);		
+
+		// $NAME$
+		replace_multiple_string(source, "$NAME$", name);
+
+		// $TYPE_CHECK$
+		replace_multiple_string(source, "$TYPE_CHECK$", type_check);
+
+		final_source += source;
+
+		exec_task += "\t\tTask" + name + " * task" + to_string(i) + " = new Task" + name + 
+			"(" + call_args + ");\n\t\tthis->executeTask(task" + to_string(i) + ");\n";
+	}
+
+	return final_source;
 }
 
 string getTypeCast(string type) {
