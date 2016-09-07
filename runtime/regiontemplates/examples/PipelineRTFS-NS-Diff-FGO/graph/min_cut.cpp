5768b823a7d847608f6dc8faa7e8541dc22fb9a4
Willian <guns945@gmail.com>
2016-09-07 01:32:31 -0300

    Stoer-Wagner min-cut algorithm implemented.
    As far as it was tested it doesn't present any bug.
    In order to compile it the boost lib is required.
    The output isn't still finished (i.e outputs on stdout
    only). Also, the cut weight ins't implemented yet, so
    the cut value on the output is a miscalculated value.
    
    The min-cut algorithm isn't integrated on the system yet.

---
+++ runtime/regiontemplates/examples/PipelineRTFS-NS-Diff-FGO/graph/min_cut.cpp
@@ -0,0 +1,276 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <boost/heap/fibonacci_heap.hpp>
+
+/**************************************************/
+/**                Basic Entities                **/
+/**************************************************/
+
+typedef int _id_t;
+typedef int weight_t;
+
+typedef std::pair<weight_t, _id_t> node_t;
+#define _node(w,id) std::make_pair(w,id)
+
+typedef std::pair<weight_t, std::pair<std::list<_id_t>, std::list<_id_t>>> cut_t;
+#define _cut(w, s1, s2) std::make_pair(w,std::make_pair(s1,s2))
+#define _cut_w(c) c.first
+#define _cut_s1(c) c.second.first
+#define _cut_t2(c) c.second.second
+
+/**************************************************/
+/**             Fibonacci Comparator             **/
+/**************************************************/
+/** This is needed to enforce that the weight is **/
+/** ordered in a descending manner, and id is    **/
+/** ordered in a ascending manner on the         **/
+/** fibonacci heap.                              **/
+/**************************************************/
+
+struct my_comparator {
+	bool operator() (const node_t& x, const node_t& y) const {
+		if (x.first != y.first)
+			return x.first < y.first;
+		else
+			return x.second > y.second;
+	}
+	typedef node_t first_argument_type;
+	typedef node_t second_argument_type;
+	typedef bool result_type;
+};
+
+typedef boost::heap::fibonacci_heap<node_t, 
+	boost::heap::compare<my_comparator>> fibonacci;
+
+/**************************************************/
+/**                Funs Prototypes               **/
+/**************************************************/
+
+int min_cut(size_t n, weight_t** adjMat);
+cut_t min_cut_phase(size_t n, weight_t** adjMat, 
+	std::map<_id_t,std::list<_id_t>> &vertices, _id_t a);
+
+weight_t sum(size_t n, weight_t* A);
+
+/**************************************************/
+/**                   Main code                  **/
+/**************************************************/
+
+int main() {
+	size_t n=5;
+	weight_t _adjMat[n][n]={{0,2,2,1,1},
+							{2,0,2,1,1},
+							{2,2,0,1,1},
+							{1,1,1,0,3},
+							{1,1,1,3,0}};
+	// weight_t _adjMat[n][n]={{0,2,0,0,3,0,0,0},
+	// 						{2,0,3,0,2,2,0,0},
+	// 						{0,3,0,4,0,0,2,0},
+	// 						{0,0,4,0,0,0,2,2},
+	// 						{3,2,0,0,0,3,0,0},
+	// 						{0,2,0,0,3,0,1,0},
+	// 						{0,0,2,2,0,1,0,3},
+	// 						{0,0,0,2,0,0,3,0}};
+	
+	// my compiler wont plat VLA, so we need this workaround
+	weight_t** adjMat = new weight_t *[n];
+	for (size_t i=0; i<n; i++) {
+		adjMat[i] = new weight_t [n];
+		for (size_t j=0; j<n; j++)
+			adjMat[i][j] = _adjMat[i][j];
+	}
+
+	min_cut(n, adjMat);
+
+}
+
+int min_cut(size_t n, weight_t** adjMat) {
+	
+	_id_t a;
+	cut_t cut;
+	std::list<cut_t> cuts;
+
+	// initialize vertices
+	std::map<_id_t, std::list<_id_t>> vertices;
+	for (int i=0; i<n; i++)
+		vertices[i] = std::list<_id_t>(1,i);
+
+	// keep finding cuts until there are no more find
+	while (vertices.size() > 1) {
+		// get the first 'a' vertex as the most connected vertex
+		weight_t max = 0;
+		weight_t m;
+		for (int i=0; i<n; i++) {
+			if ((m = sum(n, adjMat[i])) > max) {
+				a = i;
+				max = m;
+			}
+		}
+
+		// run a phase of cut
+		cuts.emplace_back(min_cut_phase(n, adjMat, vertices, a));
+		std::cout << std::endl;
+	}
+}
+
+cut_t min_cut_phase(size_t n_max, weight_t** adjMat, 
+		std::map<_id_t,std::list<_id_t>> &vertices, _id_t a) {
+	
+	_id_t s;
+	_id_t t;
+	weight_t cut;
+	size_t n = vertices.size();
+
+	// generate priority queue
+	fibonacci q;
+	for (std::pair<_id_t, std::list<_id_t>> v : vertices) {
+		if (v.first != a) {
+			q.push(_node(adjMat[v.first][a], v.first));
+		}
+	}
+
+	// array of vertices ids A
+	_id_t A[n];
+	A[0] = a;
+	t = a;
+
+	// std::cout << "a=" << a << std::endl; 
+	// greedy insertion loop
+	for (size_t i=1; i<n; i++) {
+		// add the most tightly connected vertex to A
+		// std::cout << "popping " << q.top().second << " of cost " 
+		// 	<< q.top().first << std::endl;
+		A[i] = q.top().second;
+		q.pop();
+
+		// The update queue must be done in two steps:
+		//  1 - calculate updated values for the heap
+		//  2 - actually updating the heap
+		// This must be done because when calling 'increase' the 
+		// iterator for the heap is invalidated, causing
+		// undefined behaviour.
+
+		// 1 - calculate updated values for the heap
+		fibonacci::handle_type fibonacci_handlers[n-i-1];
+		node_t fibonacci_updated[n-i]; 
+		int j=0;
+		for (fibonacci::iterator it=q.begin(); it!=q.end(); it++) {
+			fibonacci_handlers[j] = q.s_handle_from_iterator(it);
+			fibonacci_updated[j] = _node(it->first+adjMat[A[i]][it->second], it->second);
+			// std::cout << "updated: " << fibonacci_updated[j].second << ":" 
+			// 	<< fibonacci_updated[j].first << std::endl;
+			j++;
+		}
+
+		// 2 - actually updates the heap
+		for (j=0; j<n-i-1; j++) {
+			q.increase(fibonacci_handlers[j], fibonacci_updated[j]);
+		}
+
+		// update the two last added vertices
+		s = t;
+		t = A[i];
+	}
+
+	// get cut weight
+	cut = adjMat[s][t];
+
+	// std::cout << "s=" << s << std::endl;
+	// std::cout << "t=" << t << std::endl;
+
+	// std::cout << "before merge:" << std::endl;
+	// for (size_t i=0; i<n_max; i++) {
+	// 	for (size_t j=0; j<n_max; j++) {
+	// 		std::cout << adjMat[i][j] << "\t";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+
+	// generate cut
+	std::list<_id_t> s1 = vertices.at(s);
+	std::list<_id_t> s2 = vertices.at(t);
+
+	std::cout << "Got cut " << cut << ":" << std::endl;
+	std::cout << "\tS1:" << std::endl;
+	for (_id_t i=0; i<n_max; i++)
+		if (i != t && vertices.count(i) > 0)
+			for (_id_t j : vertices.at(i))		
+				std::cout << "\t\t" << j << std::endl;
+	std::cout << "\tS2:" << std::endl;
+	for (_id_t i : s2)
+		std::cout << "\t\t" << i << std::endl;
+
+	// merge s and t
+	for (std::pair<_id_t,std::list<_id_t>> _t : vertices) {
+		if (_t.first == t) {
+			// add all of t's merged vertices to s
+			vertices.at(s).insert(vertices.at(s).begin(), 
+				_t.second.begin(), _t.second.end());
+			// remove t
+			vertices.erase(_t.first);
+			break;
+		}
+	}
+
+	// update adjMat by merging the edges of t to s
+	for (size_t i=0; i<n_max; i++) {
+		for (size_t j=0; j<n_max; j++) {
+			if (i!=j) {
+				if (i == s) {
+					// std::cout << i << ":" << j << "= " << adjMat[i][j] << " with " 
+					// 	<< t << ":" << j << "= " << adjMat[t][j] << std::endl;
+					adjMat[i][j] += adjMat[t][j];
+					adjMat[j][i] += adjMat[t][j];
+				}
+			}
+		}
+	}
+
+	// remove t edges
+	for (size_t i=0; i<n_max; i++) {
+		for (size_t j=0; j<n_max; j++) {
+			if (i == t || j == t) {
+				// std::cout << i << ":" << j << "= " << adjMat[i][j] << std::endl;
+				adjMat[i][j] = 0;
+				adjMat[j][i] = 0;
+			}
+		}
+	}
+
+	// std::cout << std::endl << "after merge:" << std::endl;
+	// for (size_t i=0; i<n_max; i++) {
+	// 	for (size_t j=0; j<n_max; j++) {
+	// 		std::cout << adjMat[i][j] << "\t";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+
+	return _cut(cut, s1, s2);
+}
+
+weight_t sum(size_t n, weight_t* A) {
+	weight_t sum = 0;
+	for (int i=0; i<n; i++)
+		sum+=A[i];
+	return sum;
+}
+
+/**************************************************/
+/**              Debugging Functions             **/
+/**************************************************/
+
+void pv(std::map<_id_t,std::list<_id_t>> vertices) {
+	for (std::pair<_id_t,std::list<_id_t>> v : vertices) {
+		std::cout << v.first << std::endl;
+		for (_id_t id : v.second)
+			std::cout << "\t" << id << std::endl;
+	}
+}
+
+void pf(fibonacci q) {
+	for (fibonacci::ordered_iterator i=q.ordered_begin(); i!=q.ordered_end(); i++) {
+		std::cout << i->second << ":" << i->first << std::endl;
+	}
+}
