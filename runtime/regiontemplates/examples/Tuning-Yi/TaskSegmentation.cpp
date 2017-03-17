3c5ddc6328c538b3930d8692618ed6d337322789
George Teodoro <glmteodoro@gmail.com>
2017-03-17 17:24:30 -0300

    Adding multideclumping to RT and reverting changes in tasks

--- runtime/regiontemplates/examples/Tuning-Yi/TaskSegmentation.cpp
+++ runtime/regiontemplates/examples/Tuning-Yi/TaskSegmentation.cpp
@@ -42,8 +42,8 @@ bool TaskSegmentation::run(int procType, int tid) {
     if (inputImage.rows > 0)
         outMask = ImagenomicAnalytics::TileAnalysis::processTileCV(inputImage, otsuRatio, curvatureWeight, sizeThld,
                                                                    sizeUpperThld, mpp, mskernel,
-                                                                   levelSetNumberOfIteration);
-                                                                 //levelSetNumberOfIteration, declumpingType);
+                           //                                        levelSetNumberOfIteration);
+                                                                 levelSetNumberOfIteration, declumpingType);
 
         //Declumping types
         //0) mean-shift declumping
