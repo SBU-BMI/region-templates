8c499ee2840344b1654b878abdb7a1d0924c567a
georgeteodoro <glmteodoro@gmail.com>
2017-05-31 14:26:48 +0000

    Adding modified run.sh that deals w/ multiple concurrent executions and reports unsuccessfull executions

--- runtime/regiontemplates/examples/Tuning-Yi/run.sh
+++ runtime/regiontemplates/examples/Tuning-Yi/run.sh
@@ -42,8 +42,13 @@ while getopts "u:i:f:d:m:t:r:x:" opt; do
     esac
 done
 
-command="mpirun --allow-run-as-root -n 2 ./Tuning-Yi -i $input_image_folder -f $tuning_algorithm -d $declumpin_method -m $quality_weight -t $time_weight -r $max_iterations -x $comparison_metric"
+command="mpirun --allow-run-as-root -n 2 ../Tuning-Yi -i $input_image_folder -f $tuning_algorithm -d $declumpin_method -m $quality_weight -t $time_weight -r $max_iterations -x $comparison_metric"
 
+mkdir $exec_id
+cd $exec_id
+cp ../rtconf.xml .
+sed -i 's/\/tmp/tmp/g' rtconf.xml
+mkdir tmp
 
 start_time=`date`
 echo "STARTING execution. Command: $command"
@@ -54,10 +59,13 @@ end_time=`date`
 
 # genarate JASON file
 echo "{" > $exec_id.json
-echo "\"executionid\":$exec_id," >> $exec_id.json
-echo "\"startdatetime\":$start_time," >> $exec_id.json
-echo "\"enddatetime\":$end_time," >> $exec_id.json
-grep RESULT outtuning.txt | awk '{print $3":\""$4"\",\n"$5":\""$6"\",\n"$7":\""$8"\",\n"$9":\""$10"\",\n"$11":\""$12"\",\n"$13":\""$14"\",\n"$15":\""$16"\"" }' >> $exec_id.json
+echo "\"executionid\":\"$exec_id\"," >> $exec_id.json
+echo "\"startdatetime\":\"$start_time\"," >> $exec_id.json
+echo "\"enddatetime\":\"$end_time\"," >> $exec_id.json
+grep RESULT outtuning.txt | awk 'BEGIN{found=0}{print "\""$3"\":\""$4"\",\n""\""$5"\":\""$6"\",\n""\""$7"\":\""$8"\",\n""\""$9"\":\""$10"\",\n\""$11"\":\""$12"\",\n\""$13"\":\""$14"\",\n\""$15"\":\""$16"\""; found=1 }END{if(found==0)print "\"curvatureWeight\":\"-1\",\n\"levelSetNumberOfIteration\":\"-1\",\n\"mskernel\":\"-1\",\n\"otsuRatio\":\"-1\",\n\"sizeThld\":\"-1\",\n\"sizeUpperThld\":\"-1\""}' >> $exec_id.json
+#grep RESULT outtuning.txt | awk 'BEGIN{found=0}{print \",$3\"":\""$4"\",\n"$5":\""$6"\",\n"$7":\""$8"\",\n"$9":\""$10"\",\n"$11":\""$12"\",\n"$13":\""$14"\",\n"$15":\""$16"\""; found=1 }END{if(found==0)print "\"curvatureWeight\":\"-1\",\n\"levelSetNumberOfIteration\":\"-1\",\n\"mskernel\":\"-1\",\n\"otsuRatio\":\"-1\",\n\"sizeThld\":\"-1\",\n\"sizeUpperThld\":\"-1\"  "}' >> $exec_id.json
+#grep RESULT outtuning.txt | awk 'BEGIN{found=0}{print $3":\""$4"\",\n"$5":\""$6"\",\n"$7":\""$8"\",\n"$9":\""$10"\",\n"$11":\""$12"\",\n"$13":\""$14"\",\n"$15":\""$16"\""; found=1 }END{if(found==0)print "curvatureWeight:\"-1\",levelSetNumberOfIteration:\"-\1",mpp:\"-1\",mskernel:\"-1\",otsuRatio:\"-1\",sizeThld:\"-1\",sizeUpperThld:\"-1\""}' >> $exec_id.json
+
 echo "}" >> $exec_id.json
 
 
