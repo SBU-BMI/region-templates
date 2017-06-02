b754efd1c2b7ecdb3b2c11fe34e37f515f6bd608
Willian <guns945@gmail.com>
2017-06-02 00:53:28 -0300

    First finished version of dynablaster Bug: the final task count is wrong

--- runtime/regiontemplates/examples/PipelineRTFS-NS-Diff-FGO/fg_reuse/dynablaster.cpp
+++ runtime/regiontemplates/examples/PipelineRTFS-NS-Diff-FGO/fg_reuse/dynablaster.cpp
@@ -12,6 +12,12 @@ list<list<PipelineComponentBase*>> db_merging(
 	// R2dVL<int> cost_m(stages_to_merge.size());
 	R2dVL cost_m(stages_to_merge.size());
 
+	// convert the list to a vector for ease of access
+	vector<PipelineComponentBase*> stages{begin(stages_to_merge), end(stages_to_merge)};
+	generate_cost_m(cost_m, stages, args, ref);
+	cost_m.print_matrix();
+
+
 	// testing functions
 	// cost_m.print_list();
 	// cout << endl;
@@ -40,18 +46,197 @@ list<list<PipelineComponentBase*>> db_merging(
 	// cost_m.print_list();
 	// cout << endl;
 
-
+	// initialize solution map, containing a list of stages to be merged
+	// for each stage (containing itself)
+	map<int, pair<list<int>, PipelineComponentBase*>> merging_solution;
+	for (int i=0; i<stages_to_merge.size(); i++) {
+		pair<list<int>, PipelineComponentBase*> s;
+		s.first.emplace_back(i);
+		s.second = stages[i]->clone();
+		merging_solution[i] = s;
+	}
+	
 	// keep attempting to merge until merging sugestion is bad
 	while (true) {
 		// find best merging sugestion on cost matrix
+		list<pair<int, int>> sugestions = get_best_merging_sug(cost_m);
+
+		cout << "Merging sugestions:" << endl;
+		for (pair<int, int> p : sugestions) {
+			cout << p.first << ":" << p.second << endl;
+		}
 
 		// break the ties (if any)
+		pair<int, int> best_sug = break_ties(sugestions);
+		cout << "Best sugestion: " << best_sug.first 
+			<< ":" << best_sug.second << endl;
 
 		// verify if merging sugestion achieves stoping condition
-
 		// if not, perform mergence and update the cost matrix
+		if (stopping_condition(best_sug, merging_solution, ref, task_limit, 14)) {
+			cout << "Stop condition" << endl;
+			break;
+		}
+
+		cout << "before:" << endl;
+		print_solution(merging_solution);
+
+		// perform symbolical mergence on the stage list
+		merging_solution[best_sug.second].first.insert(
+			merging_solution[best_sug.second].first.begin(), 
+			merging_solution[best_sug.first].first.begin(), 
+			merging_solution[best_sug.first].first.end());
+
+		// perform symbolical mergence on the PCB
+		cout << "merging of " << best_sug.second << "=" << 
+			merging_solution[best_sug.second].second->tasks.size() << 
+			" with " <<	best_sug.first << "=" << 
+			merging_solution[best_sug.first].second->tasks.size();
+		merge_stages(merging_solution[best_sug.second].second, 
+			merging_solution[best_sug.first].second, ref);
+		cout << " resulting in " << 
+			merging_solution[best_sug.second].second->tasks.size() <<
+			" with reuse=" << get_reuse_factor(stages[best_sug.first], 
+					stages[best_sug.second], args, ref) << endl;
+
+
+		// remove merged stage
+		merging_solution.erase(best_sug.first);
+
+		cout << endl << "after:" << endl;
+		print_solution(merging_solution);
+
+		// remove merged line and column
+		cost_m.remove_line(best_sug.first);
+		cost_m.remove_column(best_sug.second-1);
+
+		// update the new merged stage line
+		for (int j=1+best_sug.second; j<cost_m.dimension; j++) {
+			int inc = 7-get_reuse_factor(stages[best_sug.first], 
+					stages[best_sug.second], args, ref);
+			cost_m.increment(best_sug.second, j, inc);
+		}
+
+		// update the new merged stage column
+		for (int i=0; i<best_sug.second; i++) {
+			int inc = 7-get_reuse_factor(stages[best_sug.first], 
+					stages[best_sug.second], args, ref);
+			cost_m.increment(i, best_sug.second, inc);
+		}
+
+		cost_m.print_matrix();
+		cost_m.print_list();
+	}
+
+	cout << endl << "Solution:" << endl;
+	print_solution(merging_solution);
+
+}
+
+void generate_cost_m(R2dVL& cost_m, const vector<PipelineComponentBase*>& stages,
+	const map<int, ArgumentBase*> &args, const map<string, list<ArgumentBase*>>& ref) {
+	
+	// go though all combinations of mergence
+	for (int i=0; i<stages.size(); i++) {
+		for (int j=i+1; j<stages.size(); j++) {
+			cost_m.set(i, j, 2*stages[i]->tasks.size() -
+				get_reuse_factor(stages[i], stages[j], args, ref));
+			// cost_m.set(i, j, get_reuse_factor(stages[i], stages[j], args, ref));
+		}
 	}
 }
+
+list<pair<int, int>> get_best_merging_sug(R2dVL& cost_m) {
+	list<pair<int, int>> sugs;
+	int best_cost = INT_MAX;
+	// int best_cost = 0;
+
+	// go through all stages' cost in an ordered manner
+	for (ll_elm_t<int>* cost = cost_m.vect_list.begin; cost != NULL; cost=cost->next) {
+		if (cost->value == best_cost) {
+			// if the cost is the same as the best, add it as another sugestion
+			sugs.emplace_back(pair<int, int>(cost->i, cost->j));
+		} else if (cost->value < best_cost) {
+		// } else if (cost->value > best_cost) {
+			// if this is a new best, clear the current solution and add this
+			// as the only (or first) suggestion
+			sugs.clear();
+			sugs.emplace_back(pair<int, int>(cost->i, cost->j));
+			best_cost = cost->value;
+		}
+	}
+
+	return sugs;
+}
+
+// TEMPORARY IMPLEMENTATION
+pair<int, int> break_ties(list<pair<int, int>> sugestion) {
+	return *sugestion.begin();
+}
+
+bool stopping_condition(const pair<int, int>& best_sug, 
+	const map<int, pair<list<int>, PipelineComponentBase*>>& merging_solution, 
+	const map<string, list<ArgumentBase*>>& ref, SC_opt option, int task_limit_v) {
+
+	switch (option) {
+		case task_limit:
+			return sc_task_limit(best_sug, merging_solution, ref, task_limit_v);
+		default:
+			cout << "Bad stoping condition option" << endl;
+			exit(0);
+	}
+}
+
+bool sc_task_limit(const pair<int, int>& best_sug, 
+	const map<int, pair<list<int>, PipelineComponentBase*>>& merging_solution, 
+	const map<string, list<ArgumentBase*>>& ref, int max_tasks) {
+
+	PipelineComponentBase* curr = merging_solution.at(best_sug.second).second->clone();
+	PipelineComponentBase* old = merging_solution.at(best_sug.first).second->clone();
+	merge_stages(curr, old, ref);
+
+	bool ret = curr->tasks.size() > max_tasks;
+
+	delete curr;
+	delete old;
+	
+	return ret;
+}
+
+/*******************************************************************/
+/************************ Helper Functions *************************/
+/*******************************************************************/
+
+int list_sum(list<int> l) {
+	int sum = 0;
+	for (int e : l)
+		sum += e;
+	return sum;
+}
+
+void print_solution(map<int, pair<list<int>, PipelineComponentBase*>> merging_solution) {
+	for (pair<int, pair<list<int>, PipelineComponentBase*>> bkt : merging_solution) {
+		cout << "bucket " << bkt.first << endl;
+		cout << "\twith stages:" << endl;
+		for (int s : bkt.second.first)
+			cout << "\t\t" << s << endl;
+		cout << "\t and PCB with " << bkt.second.second->tasks.size() << " tasks" << endl;
+	}
+}
+
+void print_solution2(map<int, pair<list<int>, PipelineComponentBase*>> merging_solution) {
+	for (pair<int, pair<list<int>, PipelineComponentBase*>> bkt : merging_solution) {
+		cout << "bucket " << bkt.first << endl;
+		cout << "\twith stages:" << endl;
+		for (int s : bkt.second.first)
+			cout << "\t\t" << s << endl;
+		cout << "\t and PCB with " << bkt.second.second->tasks.size() << " tasks" << endl;
+	}
+}
+
+/*******************************************************************/
+/****************************** R2dVL ******************************/
+/*******************************************************************/
 
 // responsable for initiating 
 // template<typename T>
@@ -73,6 +258,8 @@ R2dVL::R2dVL(int dimension) {
 			// create a new element
 			new_e = new ll_elm_t<int>();
 			new_e->value = i*10+j-2;
+			new_e->i = i;
+			new_e->j = dimension-j+1;
 			new_e->prev = curr_e;
 			if (curr_e != NULL)
 				curr_e->next = new_e;
@@ -105,7 +292,7 @@ int R2dVL::get(int i, int j) {
 	return vect_list.da_matrix.at(i)->at(j-i-1)->value;
 }
 
-void R2dVL::put(int i, int j, int new_value) {
+void R2dVL::set(int i, int j, int new_value) {
 	if (j-1 < i) {
 		cout << __FILE__ << ":" << __LINE__ << " Bad use of get, j must be >= i+1" << endl;
 		exit(0);
@@ -113,20 +300,44 @@ void R2dVL::put(int i, int j, int new_value) {
 	vect_list.da_matrix.at(i)->at(j-i-1)->value = new_value;
 }
 
+void R2dVL::increment(int i, int j, int inc) {
+	if (j-1 < i) {
+		cout << __FILE__ << ":" << __LINE__ << " Bad use of get, j must be >= i+1" << endl;
+		exit(0);
+	}
+	vect_list.da_matrix.at(i)->at(j-i-1)->value = 
+		vect_list.da_matrix.at(i)->at(j-i-1)->value + inc;
+}
+
 void R2dVL::remove_line(int i) {
-	if (i == 0) {
-		vect_list.begin = vect_list.last_list.at(0)->next;
+	if (vect_list.begin == vect_list.da_matrix.at(i)->at(0)) {
+		// first line is removed: just take the next of first line's last elem
+		vect_list.begin = vect_list.last_list.at(i)->next;
 	} else {
+		// take the last element of the previous line and set its 'next' as 
+		// the i-th line last's 'next'
 		vect_list.last_list.at(i-1)->next->next = vect_list.last_list.at(i)->next;
+		// update the i-th line last's next element 'prev' to be i-th-1
+		// line last element
 		vect_list.last_list.at(i-1)->next = vect_list.last_list.at(i)->next;
-		vect_list.last_list.at(i)->next->prev = vect_list.last_list.at(i-1);
+		// in case i is the last list, no need to update the i-th+1 element
+		if (vect_list.last_list.at(i)->next != NULL)
+			vect_list.last_list.at(i)->next->prev = vect_list.last_list.at(i-1);
 	}
 }
 
 void R2dVL::remove_column(int j) {
-	for (int i=0; i<=j; i++) {
-		vect_list.da_matrix.at(i)->at(j-i)->prev->next = vect_list.da_matrix.at(i)->at(j-i)->next;
-		vect_list.da_matrix.at(i)->at(j-i)->next->prev = vect_list.da_matrix.at(i)->at(j-i)->prev;
+	for (int i=0; i<j; i++) {
+		// update prev's 'next' ref of the j-th-1 column on the i-th line
+		if (vect_list.da_matrix.at(i)->at(j-1-i)->prev != NULL) {
+			vect_list.da_matrix.at(i)->at(j-1-i)->prev->next = 
+				vect_list.da_matrix.at(i)->at(j-1-i)->next;
+		}
+		// update next's 'prev' ref of the j-th+1 column on the i-th line
+		if (vect_list.da_matrix.at(i)->at(j-1-i)->next != NULL) {
+			vect_list.da_matrix.at(i)->at(j-1-i)->next->prev = 
+				vect_list.da_matrix.at(i)->at(j-1-i)->prev;
+		}
 	}
 }
 
@@ -135,7 +346,7 @@ void R2dVL::remove_column(int j) {
 void R2dVL::print_list() {
 	ll_elm_t<int>* curr = vect_list.begin;
 	while (curr != NULL) {
-		cout << curr->value << endl;
+		cout << "[" << curr->i << ":" << curr->j << "] = " << curr->value << endl;
 		curr = curr->next;
 	}
 }
@@ -150,4 +361,13 @@ void R2dVL::print_matrix() {
 		}
 		cout << endl;
 	}
+	// cout << "---------------------------------" << endl;
+	// for (vector<ll_elm_t<int>*>* line : vect_list.da_matrix) {
+	// 	for (int i=0; i<last_size-line->size(); i++)
+	// 		cout << ".\t";
+	// 	for (ll_elm_t<int>* elem : *line) {
+	// 		cout << elem->i << ":" << elem->j << "\t";
+	// 	}
+	// 	cout << endl;
+	// }
 }
--- runtime/regiontemplates/examples/PipelineRTFS-NS-Diff-FGO/fg_reuse/dynablaster.hpp
+++ runtime/regiontemplates/examples/PipelineRTFS-NS-Diff-FGO/fg_reuse/dynablaster.hpp
@@ -1,6 +1,8 @@
 #ifndef DYNABLASTER_HPP
 #define DYNABLASTER_HPP
 
+#include <limits.h>
+
 #include <list>
 #include <map>
 #include <set>
@@ -11,14 +13,11 @@
 
 using namespace std;
 
-// Performs the stage mearging using the reuse tree algorithm
-list<list<PipelineComponentBase*>> db_merging(const list<PipelineComponentBase*>& stages_to_merge, 
-	const map<int, PipelineComponentBase*> &all_stages, int size_limit, 
-	const map<int, ArgumentBase*> &args, const map<string, list<ArgumentBase*>>& ref);
-
 template <typename T>
 struct ll_elm_t {
 	T value;
+	int i;
+	int j;
 	ll_elm_t* next;
 	ll_elm_t* prev;
 };
@@ -35,22 +34,26 @@ struct ll_t {
 	vector<ll_elm_t<T>*> last_list;
 };
 
+enum SC_opt {
+	task_limit
+};
+
 // Reductible 2d Vectoral List
 // The 2d (or matrix) structure is mirrored
 // and don't have the i=j elements
 // template <typename T> class R2dVL {
 class R2dVL {
-private:
+public:
 	int dimension;
 	// ll_t<T> vect_list;
 	ll_t<int> vect_list;
 
-public:
 	R2dVL(int dimension);
 	~R2dVL() {};
 
 	int get(int i, int j);
-	void put (int i, int j, int new_value);
+	void set (int i, int j, int new_value);
+	void increment (int i, int j, int inc);
 	void remove_line(int i);
 	void remove_column(int j);
 
@@ -58,5 +61,27 @@ public:
 	void print_matrix();
 };
 
+// Performs the stage mearging using the reuse tree algorithm
+list<list<PipelineComponentBase*>> db_merging(
+	const list<PipelineComponentBase*>& stages_to_merge, 
+	const map<int, PipelineComponentBase*> &all_stages, int size_limit, 
+	const map<int, ArgumentBase*> &args, const map<string, list<ArgumentBase*>>& ref);
+
+// Merging components
+void generate_cost_m(R2dVL& cost_m, const vector<PipelineComponentBase*>& stages,
+	const map<int, ArgumentBase*> &args, const map<string, list<ArgumentBase*>>& ref);
+list<pair<int, int>> get_best_merging_sug(R2dVL& cost_m);
+pair<int, int> break_ties(list<pair<int, int>> sugestion);
+bool stopping_condition(const pair<int, int>& best_sug, 
+	const map<int, pair<list<int>, PipelineComponentBase*>>& merging_solution, 
+	const map<string, list<ArgumentBase*>>& ref, SC_opt option, int task_limit_v);
+bool sc_task_limit(const pair<int, int>& best_sug, 
+	const map<int, pair<list<int>, PipelineComponentBase*>>& merging_solution, 
+	const map<string, list<ArgumentBase*>>& ref, int max_tasks);
+
+
+// Helper functions
+int list_sum(list<int> l);
+void print_solution(map<int, pair<list<int>, PipelineComponentBase*>> merging_solution);
 
 #endif
