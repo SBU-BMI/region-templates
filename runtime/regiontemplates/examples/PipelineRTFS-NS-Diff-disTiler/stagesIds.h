f3a2bdd78104db452717f6bf6d47772afef09288
Willian <guns945@gmail.com>
2019-04-03 13:46:47 -0300

    Forgot to add header file.

---
+++ runtime/regiontemplates/examples/PipelineRTFS-NS-Diff-disTiler/stagesIds.h
@@ -0,0 +1,13 @@
+#ifndef STAGES_ID_H
+#define STAGES_ID_H
+
+static const int INITIAL_DDR_INPUT_ID = 0;
+static const std::string INITIAL_DDR_INPUT_NAME = "initial";
+static const int NORM_DDR_OUTPUT_ID = 1;
+static const std::string NORM_DDR_OUTPUT_NAME = "normalized";
+static const int SEGM_DDR_OUTPUT_ID = 2;
+static const std::string SEGM_DDR_OUTPUT_NAME = "segmented";
+// #define DIFF_DDR_OUTPUT_ID 3
+// #define DIFF_DDR_OUTPUT_NAME diff
+
+#endif
