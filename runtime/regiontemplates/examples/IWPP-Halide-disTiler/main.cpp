0fa615d96ca4c994ded7a57f660bf2d6dc105920
Willian <guns945@gmail.com>
2019-08-12 10:09:13 -0300

    Moved the iwpp implementaiton from a wrapper class to a halide external function.

--- runtime/regiontemplates/examples/IWPP-Halide-disTiler/main.cpp
+++ runtime/regiontemplates/examples/IWPP-Halide-disTiler/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 #include "cv.hpp"
 #include "Halide.h"
@@ -9,6 +10,59 @@
 
 using std::cout;
 using std::endl;
+
+extern "C" int loopedIwppRecon(halide_buffer_t* bI, halide_buffer_t* bJJ) {
+    Halide::Buffer<uint8_t> I(*bI);
+    Halide::Buffer<uint8_t> JJ(*bJJ);
+
+    Halide::Func rasterx, rastery, arasterx, arastery;
+    Halide::RDom se(-1,3,-1,3);
+    Halide::Var x, y;
+
+    // Clamping input
+    Halide::Func J = Halide::BoundaryConditions::repeat_edge(JJ);
+
+    int32_t w = bI->dim[0].extent;
+    int32_t h = bI->dim[1].extent;
+
+    // Clamping rasterx for rastery input
+    Halide::Expr xc = clamp(x, 0, w-1);
+    Halide::Expr yc = clamp(y, 0, h-1);
+    Halide::Func rasterxc;
+
+    // Functions for vertical and horizontal propagation
+    rasterx(x,y) = min(I(x,y), maximum(J(x+se.x, y)));
+    // arasterx(w-x,y) = min(I(w-x,y), maximum(rasterx(w-x+se.x, y)));
+    rasterxc(x,y) = rasterx(xc, yc);
+    rastery(x,y) = min(I(x,y), maximum(rasterxc(x, y+se.y)));
+    // arastery(x,h-y) = min(I(x,h-y), maximum(rastery(x, h-y+se.y)));
+
+    // Schedule
+    rasterx.compute_root();
+    Halide::Var xi, xo;
+    rastery.reorder(y,x).serial(y);
+    rastery.split(x, xo, xi, 16);
+    rastery.vectorize(xi).parallel(xo);
+
+    rastery.compile_jit();
+
+    int oldSum = 0;
+    int newSum = 0;
+    int it = 0;
+
+    // Create a cv::Mat wrapper for the halide pipeline output buffer
+    cv::Mat cvJ(h, w, CV_8U, JJ.get()->raw_buffer()->host);
+    do {
+        it++;
+        oldSum = newSum;
+        rastery.realize(JJ);
+        newSum = cv::sum(cvJ)[0];
+        // cout << "new - old: " << newSum << " - " << oldSum << endl;
+        // cv::imwrite("out.png", cvJ);
+    } while(newSum != oldSum);
+
+    return 0;
+}
 
 int main(int argc, char const *argv[]) {
 
@@ -32,8 +86,15 @@ int main(int argc, char const *argv[]) {
     // halCpu(x,y) = ...
     // halGpu(x,y) = ...
 
-    IwppParallelRecon iwpp;
-    iwpp.realize(cvI, cvJ);
+    // IwppParallelRecon iwpp;
+    // iwpp.realize(cvI, cvJ);
+    Halide::Buffer<uint8_t> I(cvI->data, cvI->cols, cvI->rows);
+    Halide::Buffer<uint8_t> J(cvJ->data, cvJ->cols, cvJ->rows);
+    halCpu.define_extern("loopedIwppRecon", {I, J}, Halide::UInt(8), 2);
+    cv::Mat cvOut(cvJ->cols, cvJ->rows, CV_8U);
+    Halide::Buffer<uint8_t> out(cvOut.data, cvOut.cols, cvOut.rows);
+    halCpu.realize(out);
+
 
     // Schedule funcs for both cpu and gpu
     // halCpu.parallel(x);
