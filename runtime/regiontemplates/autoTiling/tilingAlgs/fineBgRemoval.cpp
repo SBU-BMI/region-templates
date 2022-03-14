602bdaa51a37050e8686166297d0cdc95e4ba044
Willian <guns945@gmail.com>
2022-03-14 20:36:47 -0300

    Implemented partition merging for fine-grain BG removal

--- runtime/regiontemplates/autoTiling/tilingAlgs/fineBgRemoval.cpp
+++ runtime/regiontemplates/autoTiling/tilingAlgs/fineBgRemoval.cpp
@@ -154,10 +154,110 @@ std::list<rect_t> fineBgRemoval(cv::Mat img, std::list<rect_t> tiles) {
             bestRects.emplace_back(bestRect);
         }
 
+        // Generation of Dense partitions
+        std::list<rect_t> denseRects;
+        auto              bgRects = toMyRectT(bestRects);
+        denseRects                = generateBackground(bgRects, curImg.cols - 1,
+                                                       curImg.rows - 1, false);
+
+        // Attempt to reduce dense partitions generated
+        int                 minLen = 50;
+        std::vector<rect_t> denseRectsVect(denseRects.begin(),
+                                           denseRects.end());
+
+        // Ignoring first and last elements for ease of iterations
+        bool changed;
+        do {
+            changed = false;
+            for (auto curRIt = std::next(denseRectsVect.begin());
+                 curRIt != std::prev(denseRectsVect.end()); curRIt++) {
+                rect_t curR = *curRIt;
+                // If the height is too short
+                if (curR.yo - curR.yi + 1 < minLen) {
+                    // Get neighbors
+                    rect_t upperR = *std::prev(curRIt);
+                    rect_t lowerR = *std::next(curRIt);
+
+                    // Get amount of added area for each expansion
+                    int expandUpCost = (curR.yo - curR.yi + 1) *
+                                       (std::abs(curR.xi - upperR.xi) +
+                                        std::abs(curR.xo - upperR.xo));
+                    int expandDownCost = (curR.yo - curR.yi + 1) *
+                                         (std::abs(curR.xi - lowerR.xi) +
+                                          std::abs(curR.xo - lowerR.xo));
+
+                    // Expand one of the compared partitions
+                    if (expandUpCost < expandDownCost) {
+                        std::prev(curRIt)->yo = curR.yo;
+                        if (curR.xi == upperR.xi)
+                            std::prev(curRIt)->xo =
+                                std::max(curR.xo, upperR.xo);
+                        else
+                            std::prev(curRIt)->xi =
+                                std::min(curR.xi, upperR.xi);
+                    } else { // Expand down
+                        std::next(curRIt)->yi = curR.yi;
+                        if (curR.xi == lowerR.xi)
+                            std::next(curRIt)->xo =
+                                std::max(curR.xo, lowerR.xo);
+                        else
+                            std::next(curRIt)->xi =
+                                std::min(curR.xi, lowerR.xi);
+                    }
+
+                    // Remove the cur expanded partition and break since
+                    // iterator is not valid anymore
+                    denseRectsVect.erase(curRIt);
+                    changed = true;
+                    break;
+                }
+
+                // If the width is too short
+                if (curR.xo - curR.xi + 1 < minLen) {
+                    // Get neighbors
+                    rect_t leftR  = *std::prev(curRIt);
+                    rect_t rightR = *std::next(curRIt);
+
+                    // Get amount of added area for each expansion
+                    int expandLeftCost = (curR.xo - curR.xi + 1) *
+                                         (std::abs(curR.yi - leftR.yi) +
+                                          std::abs(curR.yo - leftR.yo));
+                    int expandRigthCost = (curR.xo - curR.xi + 1) *
+                                          (std::abs(curR.yi - rightR.yi) +
+                                           std::abs(curR.yo - rightR.yo));
+
+                    // Expand one of the compared partitions
+                    if (expandLeftCost < expandRigthCost) {
+                        std::prev(curRIt)->xo = curR.xo;
+                        if (curR.yi == leftR.yi)
+                            std::prev(curRIt)->yo = std::max(curR.yo, leftR.yo);
+                        else
+                            std::prev(curRIt)->yi = std::min(curR.yi, leftR.yi);
+                    } else { // Expand right
+                        std::next(curRIt)->xi = curR.xi;
+                        if (curR.yi == rightR.yi)
+                            std::next(curRIt)->yo =
+                                std::max(curR.yo, rightR.yo);
+                        else
+                            std::next(curRIt)->yi =
+                                std::min(curR.yi, rightR.yi);
+                    }
+
+                    // Remove the cur expanded partition and break since
+                    // iterator is not valid anymore
+                    denseRectsVect.erase(curRIt);
+                    changed = true;
+                    break;
+                }
+            }
+        } while (changed);
+
         cv::Mat outb = cv::Mat::zeros(curImg.rows, curImg.cols, CV_8U);
         cv::drawContours(outb, contours, contours.size() - 1, cv::Scalar(255));
         cv::Mat outd = cv::Mat::zeros(curImg.rows, curImg.cols, CV_8U);
         cv::drawContours(outd, contours, contours.size() - 1, cv::Scalar(255));
+        cv::Mat outd2 = cv::Mat::zeros(curImg.rows, curImg.cols, CV_8U);
+        cv::drawContours(outd2, contours, contours.size() - 1, cv::Scalar(255));
         for (auto bestRect : bestRects) {
             std::cout << "BG: " << bestRect.x << "-"
                       << (bestRect.x + bestRect.width - 1) << ", " << bestRect.y
@@ -169,12 +269,6 @@ std::list<rect_t> fineBgRemoval(cv::Mat img, std::list<rect_t> tiles) {
                           cv::Scalar(255), 3);
         }
 
-        // Generation of Dense partitions
-        std::list<rect_t> denseRects;
-        auto              bgRects = toMyRectT(bestRects);
-        denseRects =
-            generateBackground(bgRects, outd.cols - 1, outd.rows - 1, false);
-
         for (auto r : denseRects) {
             std::cout << "dense: " << r.xi << ":" << r.xo << ", " << r.yi << ":"
                       << r.yo << "\n";
@@ -182,8 +276,16 @@ std::list<rect_t> fineBgRemoval(cv::Mat img, std::list<rect_t> tiles) {
                           cv::Scalar(255), 3);
         }
 
+        for (auto r : denseRectsVect) {
+            std::cout << "dense: " << r.xi << ":" << r.xo << ", " << r.yi << ":"
+                      << r.yo << "\n";
+            cv::rectangle(outd2, cv::Point(r.xi, r.yi), cv::Point(r.xo, r.yo),
+                          cv::Scalar(255), 3);
+        }
+
         cv::imwrite("bg.png", outb);
-        cv::imwrite("dense.png", outd);
+        cv::imwrite("denseBefore.png", outd);
+        cv::imwrite("denseAfter.png", outd2);
     }
 
     exit(88);
